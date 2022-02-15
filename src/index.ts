24c94dd4246a7afbf7ef62203d2524d9fcc98959
Abdul Alim <abdul.alim@zohocorp.com>
2022-02-15 19:03:31 +0530

    webhook test 1

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send({
-        message: 'Hello World',
+        message: 'Hello World from web hooks',
         host: os.hostname()
     });
 });
