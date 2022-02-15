2cb204cb6dd0804dc6493fae9f0f77f136871bac
Abdul Alim <abdul.alim@zohocorp.com>
2022-02-15 22:09:41 +0530

    webhook test 2

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send({
-        message: 'Webhook is awesome! :)',
+        message: 'Webhook docker is awesome! :)',
         host: os.hostname()
     });
 });
